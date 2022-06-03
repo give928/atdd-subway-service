ccf0179771717f4af55a81828e59b03b5c0bd2c3
joohokim <give928@gmail.com>
2022-06-03 16:36:52 +0900

    refactor: 지하철 구간 일급 컬렉션 도메인 상행역 검색 기능

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -1,10 +1,13 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Embeddable
 public class Sections {
@@ -17,5 +20,21 @@ public class Sections {
 
     public void add(Section section) {
         values.add(section);
+    }
+
+    public Station findUpStation() {
+        Station downStation = values.get(0).getUpStation();
+        while (downStation != null) {
+            Station finalDownStation = downStation;
+            Optional<Section> nextLineStation = values.stream()
+                    .filter(it -> it.getDownStation() == finalDownStation)
+                    .findFirst();
+            if (!nextLineStation.isPresent()) {
+                break;
+            }
+            downStation = nextLineStation.get().getUpStation();
+        }
+
+        return downStation;
     }
 }
--- src/test/java/nextstep/subway/line/domain/SectionsTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -1,17 +1,29 @@
 package nextstep.subway.line.domain;
 
+import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SectionsTest {
     @DisplayName("상행역 검색")
     @Test
     void findUpStation() {
         // given
+        Line line = new Line("분당선", "red");
+        Station upStation = new Station("상행역");
+        Station middleStation = new Station("중간역");
+        Station downStation = new Station("하행역");
+        Sections sections = new Sections();
+        sections.add(new Section(line, upStation, middleStation, 10));
+        sections.add(new Section(line, middleStation, downStation, 10));
 
         // when
+        Station findUpStation = sections.findUpStation();
 
         // then
+        assertThat(findUpStation).isEqualTo(upStation);
     }
 
     @DisplayName("역 목록 조회")

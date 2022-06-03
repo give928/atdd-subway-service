47412e8ab6be52f65fb64e910a45c4cdc54a7fed
joohokim <give928@gmail.com>
2022-06-03 16:50:27 +0900

    test: 지하철 노선 도메인 단위 테스트 목록 작성

---
+++ src/test/java/nextstep/subway/line/domain/LineTest.java
@@ -0,0 +1,46 @@
+package nextstep.subway.line.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class LineTest {
+    @DisplayName("지하철 노선에 등록된 역 목록 조회")
+    @Test
+    void getStations() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철 노선 수정")
+    @Test
+    void update() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철 노선에 구간 추가")
+    @Test
+    void addLineStation() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("지하철 노선에 구간 삭제")
+    @Test
+    void removeLineStation() {
+        // given
+
+        // when
+
+        // then
+    }
+}

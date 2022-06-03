74719822210029f91a53b1de393408ba40f40574
joohokim <give928@gmail.com>
2022-06-03 16:19:55 +0900

    test: 지하철 구간 일급 컬렉션 도메인 단위 테스트 목록 작성

---
+++ src/test/java/nextstep/subway/line/domain/SectionsTest.java
@@ -0,0 +1,46 @@
+package nextstep.subway.line.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SectionsTest {
+    @DisplayName("상행역 검색")
+    @Test
+    void findUpStation() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("역 목록 조회")
+    @Test
+    void getStations() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("구간 추가")
+    @Test
+    void add() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @DisplayName("구간 삭제")
+    @Test
+    void remove() {
+        // given
+
+        // when
+
+        // then
+    }
+}

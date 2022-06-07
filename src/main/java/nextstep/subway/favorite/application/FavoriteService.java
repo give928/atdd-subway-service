409b635b1e56002241fe2979213d95ac521cd432
joohokim <give928@gmail.com>
2022-06-07 14:53:10 +0900

    feat: 즐겨찾기 service

---
+++ src/main/java/nextstep/subway/favorite/application/FavoriteService.java
@@ -0,0 +1,53 @@
+package nextstep.subway.favorite.application;
+
+import nextstep.subway.auth.application.AuthorizationException;
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.application.MemberService;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class FavoriteService {
+    private static final String ERROR_MESSAGE_NOT_OWNER = "즐겨찾기의 소유자만 삭제할 수 있습니다.";
+
+    private final FavoriteRepository favoriteRepository;
+    private final MemberService memberService;
+    private final StationService stationService;
+
+    public FavoriteService(FavoriteRepository favoriteRepository, MemberService memberService, StationService stationService) {
+        this.favoriteRepository = favoriteRepository;
+        this.memberService = memberService;
+        this.stationService = stationService;
+    }
+
+    public FavoriteResponse createFavorite(Long memberId, FavoriteRequest favoriteRequest) {
+        Member member = memberService.findById(memberId);
+        Station source = stationService.findStation(favoriteRequest.getSource());
+        Station target = stationService.findStation(favoriteRequest.getTarget());
+        Favorite favorite = favoriteRepository.save(new Favorite(member, source, target));
+        return FavoriteResponse.of(favorite);
+    }
+
+    public List<FavoriteResponse> findMemberFavorites(Long memberId) {
+        return favoriteRepository.findByMemberId(memberId).stream()
+                .map(FavoriteResponse::of)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteFavorite(Long memberId, Long id) {
+        Favorite favorite = favoriteRepository.findById(id)
+                .orElseThrow(RuntimeException::new);
+        if (!favorite.isOwner(memberId)) {
+            throw new AuthorizationException(ERROR_MESSAGE_NOT_OWNER);
+        }
+        favoriteRepository.delete(favorite);
+    }
+}
---
+++ src/test/java/nextstep/subway/favorite/application/FavoriteServiceTest.java
@@ -0,0 +1,96 @@
+package nextstep.subway.favorite.application;
+
+import nextstep.subway.favorite.domain.Favorite;
+import nextstep.subway.favorite.domain.FavoriteRepository;
+import nextstep.subway.favorite.dto.FavoriteRequest;
+import nextstep.subway.favorite.dto.FavoriteResponse;
+import nextstep.subway.member.application.MemberService;
+import nextstep.subway.member.domain.Member;
+import nextstep.subway.station.application.StationService;
+import nextstep.subway.station.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class FavoriteServiceTest {
+    @InjectMocks
+    private FavoriteService favoriteService;
+    @Mock
+    private FavoriteRepository favoriteRepository;
+    @Mock
+    private MemberService memberService;
+    @Mock
+    private StationService stationService;
+
+    private Station source;
+    private Station target;
+    private Member member;
+    private Favorite favorite;
+
+    @BeforeEach
+    public void setUp() {
+        source = new Station(1L, "교대역");
+        target = new Station(2L, "강남역");
+        member = new Member(1L, "test1@test.com", "password", 20);
+        favorite = new Favorite(1L, member, source, target);
+    }
+
+    @DisplayName("즐겨찾기 생성")
+    @Test
+    void createFavorite() {
+        // given
+        given(memberService.findById(member.getId())).willReturn(member);
+        given(stationService.findStation(source.getId())).willReturn(source);
+        given(stationService.findStation(target.getId())).willReturn(target);
+        given(favoriteRepository.save(any())).willReturn(favorite);
+
+        // when
+        FavoriteResponse favoriteResponse = favoriteService.createFavorite(member.getId(), new FavoriteRequest(source.getId(), target.getId()));
+
+        // then
+        assertThat(favoriteResponse.getSource().getId()).isEqualTo(source.getId());
+        assertThat(favoriteResponse.getTarget().getId()).isEqualTo(target.getId());
+    }
+
+    @DisplayName("즐겨찾기 목록 조회")
+    @Test
+    void findMemberFavorites() {
+        // given
+        given(favoriteRepository.findByMemberId(member.getId())).willReturn(Collections.singletonList(favorite));
+
+        // when
+        List<FavoriteResponse> favoriteResponses = favoriteService.findMemberFavorites(1L);
+
+        // then
+        assertThat(favoriteResponses).hasSize(1);
+        assertThat(favoriteResponses.get(0).getSource().getId()).isEqualTo(source.getId());
+        assertThat(favoriteResponses.get(0).getTarget().getId()).isEqualTo(target.getId());
+    }
+
+    @DisplayName("즐겨찾기 삭제")
+    @Test
+    void deleteFavorite() {
+        // given
+        given(favoriteRepository.findById(favorite.getId())).willReturn(Optional.of(favorite));
+
+        // when
+        favoriteService.deleteFavorite(favorite.getMember().getId(), favorite.getId());
+
+        // then
+        List<FavoriteResponse> favorites = favoriteService.findMemberFavorites(favorite.getMember().getId());
+        assertThat(favorites).isEmpty();
+    }
+}
